dfc2f79297d2a4e340ef7f6942614e5ce7c3b865
+ <+>
2024-01-08 13:59:34 +0300

    completes assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
+    def __init__(self, first_name, last_name,) -> None:
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+
         pass
--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,29 @@ from user import User
 
 import random
 
+
+
 class Teacher(User):
+    def __init__(self, first_name, last_name) -> None:
+        super().__init__(first_name, last_name)
+        self.knowledge =  [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
+         # Check if the knowledge list is not empty
+        if self.knowledge:
+            # Generate a random index within the range of the knowledge list
+            random_index = random.randint(0, len(self.knowledge) - 1)
+            # Return the random element from knowledge
+            return self.knowledge[random_index]
+        else:
+            return "I have no knowledge to share."
         pass
